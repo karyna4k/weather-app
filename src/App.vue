800642d76b6be3e1724107ed0272d739da4e23c9
karyna.kananovich <karyna4k.dev@gmail.com>
2023-08-28 15:31:13 +0400

    feat: update App file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="weather">
-    <div class="tw-container">
-      <router-view />
-    </div>
+    <router-view />
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss">
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
+<style lang="scss" scoped>
 .weather {
   @apply tw-flex tw-flex-col tw-font-Montserrat tw-bg-white;
 }
