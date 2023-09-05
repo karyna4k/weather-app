0b78327e7ee8e56a5a400ee29642213a4718a944
karyna.kananovich <karyna4k.dev@gmail.com>
2023-09-05 22:53:29 +0400

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,30 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
 export default defineConfig({
-  plugins: [vue({})],
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  build: {
+    cssCodeSplit: false,
+    target: 'esnext',
+    lib: {
+      entry: '/src/main.ts',
+      name: 'WeatherWidget',
+      fileName: (format) => `weather-widget.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  // define: {
+  //   'process.env.NODE_ENV': 'production'
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
